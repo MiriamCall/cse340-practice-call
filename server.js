0e032e80f1b1a747dc7fe1470229089f3d4daec1
MiriamCall <bow18005@byui.edu>
2025-01-21 19:43:12 -0700

    Add EJS view engine and create basic routing with static files

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
 // Import espress using ESM syntax
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+// import { path.dirname } from "path";
 
 //create an instance of an Express application
 const app = express();
 
-const name = process.env.NAME;
+// Create __dirname and __filename variables
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  const title = "Home Page";
+  const content = "<h1>Welcome to the Home Page</h1>";
+  res.render("index", { title, content });
+});
+
+app.get("/page1", (req, res) => {
+  const title = "Page 1";
+  const content = "<h1>Welcome to Page 1</h1>";
+  res.render("page1", { title: "Page 1" });
+});
+
+app.get("/page2", (req, res) => {
+  const title = "Page 2";
+  const content = "<h1>Welcome to Page 2</h1>";
+  res.render("page2", { title: "Page 2" });
+});
+
+// const name = process.env.NAME;
 
 //Define a route handler for the root URL ('/)
 app.get("/", (req, res) => {
-  res.send(`Hello, ${name}!`);
+  res.send(`Hello`);
 });
 
 //Define a route handler for the root URL ('/')
